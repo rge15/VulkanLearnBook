191686c8aaae6531476dbba1c6f788db8ce5b393
rge15 <rge15.ua@gmail.com>
2022-10-14 12:53:05 +0200

    [doc] Documentation II - More comments than before

--- src/engine/VKObjects/Managers/DeviceManager.hpp
+++ src/engine/VKObjects/Managers/DeviceManager.hpp
@@ -36,34 +36,74 @@ namespace Graphics::Manager
 
 		public:
 
-			Device( Instance& p_vkInstance, GLFWwindow& p_window )	noexcept;
+			/** @brief Default constructor of class Device */
+			explicit Device( Instance& p_vkInstance, GLFWwindow& p_window )	noexcept;
 
+			/** @brief Default destructor of class Device */
 			~Device() noexcept;
-
 			//? Ir haciendo los getters en ir haciendo falta
 
 		private:
 
+			/**
+			 * 	@brief Gets the physical device that best fits the app requeriments
+			*/
 			void getPhysicalDevice() noexcept;
 
+			/**
+			 * 	@brief Checks all the requeriments for the passed physical device
+			 * 
+			 * 	@param p_device Physical device to check
+			*/
 			bool getSuitableDevice( VkPhysicalDevice& p_device ) noexcept;
 
+			/**
+			 * 	@brief Checks all the queue suitability requeriments for the passed physical device
+			 * 
+			 * 	@param p_device Physical device to check
+			*/
 			bool checkQueuesSuitability( VkPhysicalDevice& p_device ) noexcept;
 
+			/**
+			 * 	@brief Checks all the extensions suitability requeriments for the passed physical device
+			 * 
+			 * 	@param p_device Physical device to check
+			*/
 			bool checkExtensionsSuitability( VkPhysicalDevice& p_device ) noexcept;
 
+			/**
+			 * 	@brief Inits the features requeriments for the logical device
+			*/
 			void initFeatureRequeriments() noexcept;
 
+			/**
+			 * 	@brief Sets the queue creation info
+			*/
 			void initQueueCreateInfo() noexcept;
 
+			/**
+			 * 	@brief Sets the logical device creation info
+			*/
 			void initCreationInfo() noexcept;
 
+			/**
+			 * 	@brief Creates the Vulkan logical Device
+			*/
 			void createDevice() noexcept;
 
+			/**
+			 * 	@brief Updates all the related queue data
+			*/
 			void updateQueueData() noexcept;
 			
+			/**
+			 * 	@brief Gets the Vulkan Surface to render in
+			*/
 			void getSurfaceDisplay() noexcept;
 
+			/**
+			 *	@brief Create swapchain related to the logical device
+			*/
 			void createSwapchain() noexcept;
 	};
 }
--- src/engine/VKObjects/Managers/utils/SwapchainInfo.hpp
+++ src/engine/VKObjects/Managers/utils/SwapchainInfo.hpp
@@ -20,20 +20,40 @@ namespace Graphics::Manager
 
 		VkSwapchainKHR _swapchain { VK_NULL_HANDLE };
 
-		void
-		cleanSupportInfo() noexcept;
-
+		/**
+		 * 	@brief Checks if the physical device fits the swapchain and surface requeriments
+		 * 
+		 * 	@param p_phDevice Vulkan physical device to check with
+		 * 	@param p_surface Surface to check with the properties
+		*/
 		bool
 		checkSuitableDevice( VkPhysicalDevice& p_phDevice, VkSurfaceKHR& p_surface ) noexcept;
 
+		/**
+		 * 	@brief Set the swapchain final format configuration
+		*/
 		void
 		getFinalFormat() noexcept;
 
+		/**
+		 * 	@brief Set the swapchain final present mode configuration
+		*/
 		void
 		getFinalPresentMode() noexcept;
 
+		/**
+		 * 	@brief Set the swapchain final extent configuration
+		*/
 		void
 		getFinalExtent( GLFWwindow& p_window ) noexcept;
+
+		private:
+
+		/**
+		 * 	@brief Cleans all the swapchain support related info
+		*/
+		void
+		cleanSupportInfo() noexcept;
 
 	};
 }
